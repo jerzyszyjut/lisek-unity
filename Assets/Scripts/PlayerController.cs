c3d297169e8a577c85d47297bd32130a371a6740
Mateusz <escobar-123@wp.pl>
2023-10-29 09:56:29 +0100

    Update PlayerController

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,4 +1,4 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -6,29 +6,44 @@ public class PlayerController : MonoBehaviour
 {
     [Header("Movement parameters")]
     [Range(0.01f, 20.0f)] [SerializeField] private float moveSpeed = 0.1f; // move speed
-    [Range(0.01f, 20.0f)] [SerializeField] private float jumpForce = 6.0f;
+    [Range(0.01f, 20.0f)][SerializeField] private float jumpForce = 6.0f;
     private readonly float rayLength = 1.0f;
     public LayerMask groundLayer;
+    //public Vector3 theScale; //MF III 13 a)
     private Rigidbody2D rigidBody;
     private Animator animator; //MF
     private bool isWalking = false; //MF
+    //posta� zawr�cena w prawo = true, w lewo = false
+    private bool isFacingRight = true; //MF
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
         isWalking = false; //MF
-        if(Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D)) 
+        if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D))
         {
+            //MF
+            if (!isFacingRight)
+            {
+                isFacingRight = true;
+                Debug.Log("Going Right");
+            }
             isWalking = true; //MF
             transform.Translate(moveSpeed * Time.deltaTime, 0.0f, 0.0f, Space.World);
         }
         if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A))
         {
+            //MF
+            if (isFacingRight)
+            {
+                isFacingRight = false;
+                Debug.Log("Going Left");
+            }
             isWalking = true; //MF
             transform.Translate(-1 * moveSpeed * Time.deltaTime, 0.0f, 0.0f, Space.World);
         }
@@ -48,9 +63,17 @@ public class PlayerController : MonoBehaviour
         animator = GetComponent<Animator>(); //MF 
     }
 
+    //MF - ca�a metoda
+    void Flip()
+    {
+        isFacingRight = !isFacingRight;
+        Vector3 theScale = transform.localScale; //???
+        theScale.x *= -1;
+        transform.localScale = theScale;
+    }
     void Jump()
     {
-        if(IsGrounded())
+        if (IsGrounded())
         {
             rigidBody.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
             Debug.Log("Jumping!");
