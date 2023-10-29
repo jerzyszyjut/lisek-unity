ee5002bc698821e136b30553261d04b6a57cf56f
Jerzy Szyjut <jerzy.szyjut@outlook.com>
2023-10-29 12:09:48 +0100

    Add jumping and falling animation

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,82 +2,101 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+enum FacingDirection
+{
+    Left,
+    Right
+}
+
 public class PlayerController : MonoBehaviour
 {
     [Header("Movement parameters")]
-    [Range(0.01f, 20.0f)] [SerializeField] private float moveSpeed = 0.1f; // move speed
+    [Range(0.01f, 20.0f)][SerializeField] private float moveSpeed = 0.1f;
     [Range(0.01f, 20.0f)][SerializeField] private float jumpForce = 6.0f;
     private readonly float rayLength = 1.0f;
     public LayerMask groundLayer;
-    //public Vector3 theScale; //MF III 13 a)
     public int score = 0;
     private Rigidbody2D rigidBody;
-    private Animator animator; //MF
-    private bool isWalking = false; //MF
-    //posta� zawr�cena w prawo = true, w lewo = false
-    private bool isFacingRight = true; //MF
-    // Start is called before the first frame update
+    private Animator animator;
+    private bool isWalking = false;
+    private bool isFalling = false;
+    private FacingDirection facingDirection = FacingDirection.Right;
+
     void Start()
     {
 
     }
 
-    //MF
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Bonus"))
         {
             other.gameObject.SetActive(false);
-            Debug.Log("Score: " + score);
             score += 1;
         }
     }
 
-    // Update is called once per frame
     void Update()
     {
-        isWalking = false; //MF
+        isWalking = false;
+
         if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D))
         {
-            //MF
-            if (!isFacingRight)
+            if (facingDirection == FacingDirection.Left)
             {
                 Flip();
             }
-            isWalking = true; //MF
+            isWalking = true;
             transform.Translate(moveSpeed * Time.deltaTime, 0.0f, 0.0f, Space.World);
         }
+
         if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A))
         {
-            //MF
-            if (isFacingRight)
+            if (facingDirection == FacingDirection.Right)
             {
                 Flip();
             }
-            isWalking = true; //MF
+            isWalking = true;
             transform.Translate(-1 * moveSpeed * Time.deltaTime, 0.0f, 0.0f, Space.World);
         }
+
         if (Input.GetMouseButtonDown(0) || Input.GetKeyDown(KeyCode.Space))
         {
             Jump();
         }
-        Debug.DrawRay(transform.position, rayLength * Vector3.down, Color.white, 1, false);
-        animator.SetBool("isGrounded", IsGrounded()); //MF
-        animator.SetBool("isWalking", isWalking); //MF
 
+        if(rigidBody.velocity.y < 0.0f)
+        {
+            isFalling = true;
+        } else
+        {
+            isFalling = false;
+        }
+
+        //Debug.DrawRay(transform.position, rayLength * Vector3.down, Color.white, 1, false);
+        animator.SetBool("isGrounded", IsGrounded());
+        animator.SetBool("isWalking", isWalking);
+        animator.SetBool("isFalling", isFalling);
     }
 
     private void Awake()
     {
         rigidBody = GetComponent<Rigidbody2D>();
-        animator = GetComponent<Animator>(); //MF 
+        animator = GetComponent<Animator>();
     }
 
-    //MF - ca�a metoda
     void Flip()
     {
-        isFacingRight = !isFacingRight;
-        Vector3 theScale = transform.localScale; //???
+        if (facingDirection != FacingDirection.Left)
+        {
+            facingDirection = FacingDirection.Left;
+        }
+        else
+        {
+            facingDirection = FacingDirection.Right;
+        }
+
+        Vector3 theScale = transform.localScale;
         theScale.x *= -1;
         transform.localScale = theScale;
     }
@@ -87,7 +106,6 @@ public class PlayerController : MonoBehaviour
         if (IsGrounded())
         {
             rigidBody.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
-            Debug.Log("Jumping!");
         }
     }
 
