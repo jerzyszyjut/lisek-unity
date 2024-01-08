ac0a4ea9656d389651dd88b2bfae840e20a5d1f2
Mateusz <escobar-123@wp.pl>
2024-01-08 04:32:32 +0100

    v1
    everything s ok

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -49,30 +49,37 @@ public class EnemyController : MonoBehaviour
         transform.Translate(movement, Space.World);
     }
 
+    private bool isDead = false;
+
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.CompareTag("Player"))
+        if (!isDead)
         {
-            if (transform.position.y < other.gameObject.transform.position.y)
+            if (other.CompareTag("Player"))
             {
-                animator.SetBool("isDead", true);
-                StartCoroutine(KillOnAnimationEnd());
+                if (transform.position.y < other.gameObject.transform.position.y)
+                {
+                    isDead = true;
+                    animator.SetBool("isDead", true);
+                    StartCoroutine(KillOnAnimationEnd());
+                }
             }
-        }
-        if(other.CompareTag("Bullet"))
-        {
-            currentHitpoints -= 40.0f;
-
-            if (currentHitpoints <= 0.0f)
+            if (other.CompareTag("Bullet"))
             {
-                animator.SetBool("isDead", true);
-                StartCoroutine(KillOnAnimationEnd());
-                GameManager.instance.IncrementEnemiesDefeated();
-                healthbar.UpdateHealthBar(0.0f);
-            }
-            else
-            {
-                healthbar.UpdateHealthBar(currentHitpoints / hitpoints);
+                currentHitpoints -= 40.0f;
+                if (currentHitpoints <= 0.0f)
+                {
+                    isDead = true;
+                    animator.SetBool("isDead", true);
+                    StartCoroutine(KillOnAnimationEnd());
+                    GameManager.instance.IncrementEnemiesDefeated();
+                    healthbar.UpdateHealthBar(0.0f);
+                }
+                else
+                {
+                    healthbar.UpdateHealthBar(currentHitpoints / hitpoints);
+                }
+                Destroy(other);
             }
         }
     }
